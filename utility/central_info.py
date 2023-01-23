fd8f0809a421c53f5b4ef4d23e956c5610d2fdbf
Rick Kauffman <rick@rickkauffman.com>
2023-01-23 16:22:11 -0700

    Delete central_info.py

--- utility/central_info.py
+++
@@ -1,9 +0,0 @@
-central_info = {
-           "username": "rick@rickkauffman.com",
-           "password": "ilike2Rock*",
-           "client_id": "lSxHkGa7ixmfe5X1YCjjN0WCkyWMhXy8",
-           "client_secret": "0R0OfTWjPYJXTcO7YeqK9YWfBV9aXeas",
-           "customer_id": "917dff368c8a11ed928e1ef279d558a5",
-           "base_url": "https://apigw-uswest4.central.arubanetworks.com",
-           "refresh_token": "G5DV3Mjunf576ULliKGVgmv5NuL8P5zd"
-       }
